22beef82f88e69e0ded5be6a393bca26aa696fe8
mounika vakacharlla <vakacharlamounika861@gmail.com>
2023-09-23 14:07:52 +0530

    Bag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Login from './Components/Login/Login';
+import Bag from './Components/Bag/Bag';
+import Address from './Components/Bag/Address';
 
 function App() {
   return (
     <div className="App app-width">
       <Login />
+      <Bag />
+      <Address />
     </div>
   );
 }
